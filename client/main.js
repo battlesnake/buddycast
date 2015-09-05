617bfb39d7bf6f3432338ca112cb2d7664d4b70b
Jurren Pen <jurrenpen@gmail.com>
2015-09-05 15:38:54 +0200

    userId on client

--- client/main.js
+++ client/main.js
@@ -1,13 +1,22 @@
 Meteor.subscribe('messages');
 
+if(typeof Session.get('userId') === 'undefined') {
+	Meteor.call('userInsert', function(err, userId) {
+		Session.setPersistent('userId', userId)
+	});
+}
+
 Template.postMessage.events({
 	'submit form': function (e, template) {
 		e.preventDefault();
 
 		var $text = $(e.target).find('[name=text]');
 		var message = {
+			userId: Session.get('userId'),
 			text: $text.val()
 		};
+
+		console.log('UserId: ', Session.get('userId'))
 
 		Meteor.call('messageInsert', message);
 	}
@@ -21,4 +30,6 @@ Template.showMessages.helpers({
 		return messages;
 	}
 })
+
+
 
